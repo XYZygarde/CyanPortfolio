fb109ef3b97efa35fb87a893a7bf794a73fbca54
Christian Ihao Ancog <137300859+XYZygarde@users.noreply.github.com>
2026-03-13 00:07:04 +0800

    finalized design

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -101,24 +101,7 @@ if (heroBg) {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('.scroll-fade');
 
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('is-visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, {
-        threshold: 0.1
-    });
-
-    sections.forEach(section => {
-        observer.observe(section);
-    });
-});
 
 
 
